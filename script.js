f4edc38693b4d5c34ea883760465611cd240b831
Golfredof <golfredof@gmail.com>
2022-09-05 19:23:22 -0400

    Dynamic eparators

--- script.js
+++ script.js
@@ -75,6 +75,25 @@ var typed = new Typed(".auto-type", {
 // Parallax banner
 
 
+const paths = [...document.querySelectorAll('path.path-anim')];
+paths.forEach(el => {
+    const svgEl = el.closest('svg');
+    const pathTo = el.dataset.pathTo;
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: svgEl,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true
+        }
+    })
+    .to(el, {
+        ease: 'none',
+        attr: { d: pathTo }
+    });
+});
+
 
 // Parallax rellax
 
